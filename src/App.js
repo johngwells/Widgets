a8e4fc53631b1465826fee5acfcbdf76227e6288
John Wells <molekewl2017@gmail.com>
2020-10-19 22:41:11 -0700

    make components for app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+  {
+    title: 'What is React?',
+    content: 'Frontend framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'favorite JS Library'
+  },
+  {
+    title: 'How do you use React',
+    content: 'make components'
+  }
+];
+
+export default () => {
+  return <h1><Accordion items={items} /></h1>
 }
-
-export default App;
